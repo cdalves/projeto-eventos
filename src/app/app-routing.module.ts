2df53066dba6fc72c97e47c4d0928b65a693b938
Carlos Daniel <cdanielalvesibce@gmail.com>
2022-10-24 14:56:07 -0300

    add tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutInicioComponent } from './components/layout-inicio/layout-inicio.component';
+import { TelaLoginComponent } from './components/tela-login/tela-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LayoutInicioComponent},
+  {path: 'login', component: TelaLoginComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
